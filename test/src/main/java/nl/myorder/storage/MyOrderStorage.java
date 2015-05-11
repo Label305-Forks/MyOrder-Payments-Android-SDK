4838d22e61943932739a60bcfcf0ab46c4082e56
Niek Haarman <haarman.niek@gmail.com>
2015-05-11 14:33:53 +0200

    Formatting

---
+++ test/src/main/java/nl/myorder/storage/MyOrderStorage.java
@@ -0,0 +1,45 @@
+package nl.myorder.storage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import myorder.MOCredentialStorage;
+
+public class MyOrderStorage implements MOCredentialStorage {
+
+rivate static final String MY_ORDER_PREFERENCE_KEY = "MyOrderpreferenceKey";
+
+	p
+	private static SharedPreferences userPreff;
+	public MyOrderStorage(Context mContext) {
+
+    f (userPreff == null) {
+			userPreff = mContext.getSharedPreferences(MY_ORDER_PREFERENCE_KEY, Context.MODE_PRIVATE);
+		}
+	}
+
+	@Override
+	public String get(String key) {
+		return userPreff.getString(key, null);
+	}
+
+	@Override
+	public void put(String key, String value) {
+		Editor edit = userPreff.edit();
+		edit.putString(key, value);
+		edit.commit();
+	}
+
+	@Override
+	public boolean remove(String key) {
+		userPreff.edit().remove(key).commit();
+		return true;
+	}
+
+	@Override
+	public boolean removeAll() {
+		userPreff.edit().clear().commit();
+		return true;
+	}
+
+}
